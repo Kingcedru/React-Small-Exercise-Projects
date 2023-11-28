0ec276c609e4f9ed32af519635466394ccbd7fa2
Kingcedru <byishimocedrick@gmail.com>
2023-11-28 11:46:52 +0200

    navabar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import ImageSlides from "./imageSlides/ImageSlides";
-import QuizApp from "./quizApp/QuizApp";
+import NavBar from "./navBar/NavBar";
 
 export default function App() {
   return (
     <>
-      <QuizApp/>
+      <NavBar/>
     </>
   )
 }
